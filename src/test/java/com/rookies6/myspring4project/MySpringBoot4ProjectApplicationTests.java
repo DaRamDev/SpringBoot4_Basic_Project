bae44162e1b3e74af5ab9932cdcb110f78bcd723
DARAM <merj4871@naver.com>
2026-08-31 14:16:39 +0900

    made main

--- src/test/java/com/rookies6/myspring4project/MySpringBoot4ProjectApplicationTests.java
+++ src/test/java/com/rookies6/myspring4project/MySpringBoot4ProjectApplicationTests.java
@@ -1,11 +1,18 @@
 package com.rookies6.myspring4project;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MySpringBoot4ProjectApplicationTests {
 
+	public static void main(String[] args) {
+		SpringBootApplication application = new SpringBootApplication(MySpringBoot4ProjectApplication.class);
+
+		application.run(args);
+
+	}
 	@Test
 	void contextLoads() {
 	}
