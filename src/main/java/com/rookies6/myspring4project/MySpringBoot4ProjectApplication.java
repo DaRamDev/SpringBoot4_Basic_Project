f4a3ab334a29fed6f58ff51b2f99a8610b778df9
DARAM <merj4871@naver.com>
2026-08-31 14:45:45 +0900

    람다

--- src/main/java/com/rookies6/myspring4project/MySpringBoot4ProjectApplication.java
+++ src/main/java/com/rookies6/myspring4project/MySpringBoot4ProjectApplication.java
@@ -1,20 +1,25 @@
 package com.rookies6.myspring4project;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.thymeleaf.web.IWebApplication;
 
 
 @SpringBootApplication
 public class MySpringBoot4ProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MySpringBoot4ProjectApplication.class, args);
+		//SpringApplication.run(MySpringBoot4ProjectApplication.class, args);
+		SpringApplication application = new SpringApplication(MySpringBoot4ProjectApplication.class);
+
+		application.setWebApplicationType(WebApplicationType.SERVLET);
+		application.run(args);
 	}
 
 	@Bean
-	public String mybean(){
+	public String myBean() {
 		return "Hello Bean";
 	}
-
 }
