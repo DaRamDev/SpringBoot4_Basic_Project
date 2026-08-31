978bd37bf2344d9a97f87cd5b68b84185fd9dbd6
DARAM <merj4871@naver.com>
2026-08-31 14:06:34 +0900

    first push in SpringBoot

---
+++ src/test/java/com/rookies6/myspring4project/MySpringBoot4ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rookies6.myspring4project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringBoot4ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
