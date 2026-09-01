fc96fcdf4f042bab52e13d45c76ef2e2c5c3ca66
DARAM <merj4871@naver.com>
2026-09-01 10:27:24 +0900

    플러그인

--- src/main/java/com/rookies6/myspring4project/config/TestConfig.java
+++ src/main/java/com/rookies6/myspring4project/config/TestConfig.java
@@ -1,4 +1,17 @@
 package com.rookies6.myspring4project.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+//@Profile("test")
 public class TestConfig {
+    @Bean
+    public CustomVO customVO() {
+        return CustomVO.builder() //CustomVOBuilder
+                .mode("테스트 환경")
+                .rate(0.5)
+                .build();
+    }
 }
